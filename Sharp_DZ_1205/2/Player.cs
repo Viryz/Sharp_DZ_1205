110ab1e2e4d49c6899d503c8f5b6ea5cac1b703f
Viryz <viryz1998@gmail.com>
2018-05-23 00:06:59 +0300

    second task

---
+++ Sharp_DZ_1205/2/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Player : IPlayble, IRecodable
+    {
+        void IPlayble.Play()
+        {
+            Console.WriteLine("IPlayble play");
+        }
+
+        void IPlayble.Pause()
+        {
+            Console.WriteLine("IPlayble pause");
+        }
+
+        void IPlayble.Stop()
+        {
+            Console.WriteLine("IPlayble stop");
+        }
+
+        void IRecodable.Record()
+        {
+            Console.WriteLine("IRecodable record");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("IRecodable pause");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("IRecodable stop");
+        }
+    }
+}
---
+++ Sharp_DZ_1205/2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            IPlayble p1;
+            IRecodable p2;
+            
+            Console.WriteLine("Enter \n1: play\n2: record");
+            try
+            {
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    p1 = player;
+                    p1.Play();
+                    p1.Pause();
+                    p1.Stop();
+                }
+                else if (choice == 2)
+                {
+                    p2 = player;
+                    p2.Record();
+                    p2.Pause();
+                    p2.Stop();
+                }
+                else throw new Exception("Wrong chioce");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
